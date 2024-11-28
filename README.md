fd6fcf4ff565a3f6d504d6f9071d33253888cfc3
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2024-11-28 21:48:37 +0100

    step 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/LiudmilaKorchikova/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/LiudmilaKorchikova/java-project-72/actions)
+### Mainainability:
+[![Maintainability](https://api.codeclimate.com/v1/badges/0b40fc097341d227ed04/maintainability)](https://codeclimate.com/github/LiudmilaKorchikova/java-project-72/maintainability)
+### Test Coverage:
+[![Test Coverage](https://api.codeclimate.com/v1/badges/0b40fc097341d227ed04/test_coverage)](https://codeclimate.com/github/LiudmilaKorchikova/java-project-72/test_coverage)
---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("info.picocli:picocli:4.6.3")
+    annotationProcessor("info.picocli:picocli-codegen:4.6.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.15.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+    }
+}
