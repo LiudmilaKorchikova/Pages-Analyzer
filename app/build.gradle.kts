7d6992c0274020022bfb732bc2f2448459699881
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2024-12-07 12:40:29 +0100

    step 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("io.javalin:javalin:6.3.0")
 
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
+
     implementation("com.h2database:h2:2.2.220")
     runtimeOnly("com.h2database:h2:2.2.224")
     runtimeOnly("org.postgresql:postgresql:42.6.0")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 
 import io.javalin.Javalin;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import io.javalin.rendering.template.JavalinJte;
+import gg.jte.resolve.ResourceCodeResolver;
 
 
 public final class App {
@@ -9,15 +13,26 @@ public final class App {
         String portEnv = System.getenv("PORT");
         int port = (portEnv != null) ? Integer.parseInt(portEnv) : 7000;
 
-        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging()).start(port);
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
+        });
 
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.start(port);
 
         return app;
     }
 
     public static void main(String[] args) throws Exception {
         Javalin app = getApp();
-        app.start();
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 }
