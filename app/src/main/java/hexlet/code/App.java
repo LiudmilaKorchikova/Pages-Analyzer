997f3aca98e6ab7dbb9298835816850dc514a7bf
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2024-12-14 02:32:28 +0100

    step6

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -48,8 +48,20 @@ public final class App {
             config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
-        String databaseUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL",
+        String databaseUrl = String.format(
+                "jdbc:postgresql://%s:%s/%s?user=%s&password=%s",
+                System.getenv("HOST"),
+                System.getenv("DB_PORT"),
+                System.getenv("DATABASE"),
+                System.getenv("USERNAME"),
+                System.getenv("PASSWORD")
+        );
+
+        if (databaseUrl == null || databaseUrl.isEmpty()) {
+            databaseUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL",
                     "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        }
+
 
 
         HikariConfig hikariConfig = new HikariConfig();
